df543fd1ce9135c55c57d5e2cbf68865100107dd
Aditya Narayan Jha <adityanarayanjha31@gmail.com>
2022-06-25 11:27:12 +0530

    added Crud for queries

--- routes/notes.js
+++ routes/notes.js
@@ -9,88 +9,44 @@ router.use(cors())
 
 //authorization required(auth-token needed as the header)
 //endpoint to get all the notes a particular user
-router.get('/getNotes', authorize, async (req, res) => {
+router.get('/getNotes', async (req, res) => {
     try {
-        //check if the user exists
-        let userInfo = await User.findById(req.user.id)
-        //if the user does not exist
-        if (!userInfo) {
-            res.status(404).send('user not found')
-        }
-
-        //if the user exists send the details of the note
-        else {
-            let data = await Note.find({ userId: req.user.id });
-            res.json(data);
-        }
-    } catch (error) {
+        let data = await Note.find();
+        res.json(data);
+    }
+    catch (error) {
         res.status(404).send('internal server error')
     }
 })
 
-//authorization required(auth-token needed as the header)
-//endpoint to create notes for a particular user
-router.post("/createNote", authorize, async (req, res) => {
+router.post("/createNote", async (req, res) => {
     try {
-        //check if the user exists
-        let userInfo = await User.findById(req.user.id)
-        //if the user does not exist
-        if (!userInfo) {
-            res.status(404).send('user not found')
-        }
-        //if the user exists create the note and send the details of the note
-        else {
             let data = {
+                email:req.body.email,
                 title: req.body.title,
-                description: req.body.description,
-                userId: req.user.id
+                description: req.body.description
             }
             let result = await Note.create(data);
-            if (result.length===0) {
+            if (result.length === 0) {
                 res.send("An error Occured");
             }
             else res.json(result);
-        }
     } catch (err) {
         res.status(404).send("Internal Server Error")
     }
 });
 
-router.put('/updateNote/:id', authorize, async (req, res) => {
+
+//authorization required(auth-token needed as the header)
+//endpoint to delete notes
+router.delete('/deleteNote/:id', authorize,async (req, res) => {
     try {
-        //check if the user exists
         let userInfo = await User.findById(req.user.id)
-        let upnote = await Note.findById(req.params.id)
-        //if the user does not exist
-        if (!userInfo || !upnote) {
-            res.status(404).send('user/note not found')
+        if (!userInfo) {
+            res.status(404).send('user not found')
         }
 
-        else if (req.user.id != upnote.userId) {
-            res.status(401).send('This action is not permitted')
-        }
-        //if the user exists create the note and send the details of the note
-        else {
-            let note = await Note.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
-            res.json(note);
-        }
-    } catch (error) {
-        res.status(404).send("Internal Server Error")
-    }
-})
-
-router.delete('/deleteNote/:id', authorize, async (req, res) => {
-    try {
-        //check if the user exists
-        let userInfo = await User.findById(req.user.id)
-        let upnote = await Note.findById(req.params.id)
-        //if the user does not exist
-        if (!userInfo || !upnote) {
-            res.status(404).send('user/note not found')
-        }
-
-        else if (req.user.id != upnote.userId) {
-
+        else if (!userInfo.isAdmin) {
             res.status(401).send('This action is not permitted')
         }
         else {
