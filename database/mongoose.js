8b001bbbdcb582a6fceffa03c71a101a5d68b5d5
Aditya Narayan Jha <adityanarayanjha31@gmail.com>
2022-06-25 13:25:32 +0530

    deployment changes

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
-let user = process.env.USER_ID;
-let key = process.env.USER_KEY;
+// require('dotenv').config();
+// let user = process.env.USER_ID;
+// let key = process.env.USER_KEY;
 
-const mongoURI = `mongodb+srv://${user}:${key}@cluster0.a1jux.mongodb.net/tabletennis?retryWrites=true&w=majority`
-// const mongoURI = process.env.MONGO_URI
+// const mongoURI = `mongodb+srv://${user}:${key}@cluster0.a1jux.mongodb.net/tabletennis?retryWrites=true&w=majority`
+const mongoURI = process.env.MONGO_URI
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
         console.log("Connected to Mongo Successfully");
